ef150253f9afa50121a7bd6012f8fd79cab8caac
malcozui <malcozui@gmail.com>
2022-05-14 11:52:27 +0200

    updated key app

--- python typer.py
+++ python typer.py
@@ -4,22 +4,31 @@ import time
 
 keyboard = Controller
 
-def press_keys(char: chr):
+def press_keys(char: str):
     keyboard.press(char)
     keyboard.release(char)
 
-def press_keycode(keycode):
+def press_keycode(keycode: Key):
     keyboard.press(keycode)
     keyboard.release(keycode)
 
-def type_string(string):
+def type_string(string: str):
     keyboard.type(string)
 
-def realistic_typing_string(string):
+def realistic_typing_string(string: str):
     for char in string:
         keyboard.press(char)
         keyboard.release(char)
         time.sleep(0.12)
 
-press_keys()
+# time delay to move my cursor to a notepad window.
+time.sleep(2)
+
+press_keys("b")
+
+press_keycode(Key.cmd)
+
+type_string("penis man")
+
+realistic_typing_string("Hello World!")
 #windows key is Key.cmd
