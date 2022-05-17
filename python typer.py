0e16715bf737be2901a2438c5c64f0eb970a701a
malcozui <malcozui@gmail.com>
2022-05-17 10:23:43 +0200

    structured a class, and made some prebuilt things to do with this power

--- python typer.py
+++ python typer.py
@@ -2,22 +2,51 @@ from turtle import st
 from pynput.keyboard import Key, Controller
 import time
 
-keyboard = Controller
+keyboard = Controller()
 
-def press_keys(char: str):
-    keyboard.press(char)
-    keyboard.release(char)
-
-def press_keycode(keycode: Key):
-    keyboard.press(keycode)
-    keyboard.release(keycode)
-
-def type_string(string: str):
-    keyboard.type(string)
-
-def realistic_typing_string(string: str):
-    for char in string:
+class Key_Functions:
+    def press_keys(char: str):
         keyboard.press(char)
         keyboard.release(char)
-        time.sleep(0.12)
 
+    def press_keycode(keycode: Key):
+        keyboard.press(keycode)
+        keyboard.release(keycode)
+
+    def type_string(string: str):
+        keyboard.type(string)
+
+    def realistic_typing_string(string: str, time_between_keystrokes: float = 0.12):
+        for char in string:
+            keyboard.press(char)
+            keyboard.release(char)
+            time.sleep(time_between_keystrokes)
+    
+    def open_application(app: str):
+        kf.press_keycode(Key.cmd)
+        time.sleep(.1)
+        kf.type_string(app)
+        time.sleep(.1)
+        kf.press_keycode(Key.enter)
+        time.sleep(.2)
+
+
+kf = Key_Functions
+
+def notepad_typer():
+    kf.open_application("notepad")
+    # Now notepad is open.
+    kf.realistic_typing_string("Whatever you'd like to type to the user.", 0.05)
+
+def cmd_open():
+    kf.open_application("cmd")
+    time.sleep(.1)
+    # Now cmd is open, not in admin though :(
+    kf.realistic_typing_string("color a && cls && cmd")
+    kf.press_keycode(Key.enter)
+    time.sleep(.1)
+    kf.type_string("tree")
+    kf.press_keycode(Key.enter)
+
+# notepad_typer()
+# cmd_open()
