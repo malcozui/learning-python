37f2caef425193fcab1bc2ecb4e8eafa479da662
malcozui <malcozui@gmail.com>
2022-05-24 18:33:52 +0200

    made a layer to output using loops

--- NeuralNetworkAttempt/layer_with_loop.py
+++ NeuralNetworkAttempt/layer_with_loop.py
@@ -1,3 +1,7 @@
+# This is a tutorial series I'm following on YT by sentdex
+# my goal is to learn how neural networks work, and theoretically i should
+# be able to do this in another lang I'm more comfortable with as well like C#
+
 inputs = [1, 2, 3, 2.5]
 
 weights = [
