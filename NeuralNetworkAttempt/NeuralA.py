5b22ad85311a12b8357e7bd75ea09806e75353eb
malcozui <malcozui@gmail.com>
2022-05-24 16:18:24 +0200

    started the neural network thing

---
+++ NeuralNetworkAttempt/NeuralA.py
@@ -0,0 +1,3 @@
+# This is a tutorial series I'm following on YT by sentdex
+# my goal is to learn how neural networks work, and theoretically i should
+# be able to do this in another lang I'm more comfortable with as well like C#
