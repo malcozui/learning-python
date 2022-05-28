76b6093996cfe79f98f963fd79be91424bee6bbc
malcozui <malcozui@gmail.com>
2022-05-28 10:01:29 +0200

    implimented softmax

--- NeuralNetworkAttempt/NeuralA.py
+++ NeuralNetworkAttempt/NeuralA.py
@@ -1,27 +1,8 @@
+from cmath import exp
 import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
 nnfs.init()
-# this is the spiral data function.
-'''
-def create_data(points, classes):
-  X = np.zeros((points * classes), 2)
-  y = np.zeros(points * classes, dtype='uint8')
-  for class_number in range(classes):
-    ix = range(points * class_number, points * (class_number + 1))
-    r = np.linspace(0.0, 1, points)
-    t = np.linspace(class_number * 4, (class_number + 1) * 4, points) + np.random.randn(points) * 0.2
-    X[ix] = np.c_[r * np.sin(t * 2.5), r * np.cos(t * 2.5)]
-    y[ix] = class_number
-  return X, y
-'''
-
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
-
-X, y = spiral_data(100, 3)
-
 class Layer_Dense:
   def __init__(self, n_inputs, n_neurons):
     # Weights are multiplied by 0.10 to keep them in range -1 < x < 1
@@ -30,16 +11,30 @@ class Layer_Dense:
     self.biases = np.zeros((1, n_neurons))
   def forward(self, inputs):
     self.output = np.dot(inputs, self.weights) + self.biases
-
 class Activation_ReLU:
   # Activation function using rectified linear equasion { x <= 0 == 0; x > 0 == x}
   def forward(self, inputs):
     self.output = np.maximum(0, inputs)
     #applies the ReLU to the inputs
+class Activation_Softmax:
+  def forward(self, inputs):
+    exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+    probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+    self.output = probabilities
 
-layer1 = Layer_Dense(2,5)
+
+X, y = spiral_data(samples = 100, classes = 3)
+
+dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
