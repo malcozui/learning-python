ed686a80f9965c1fd3492456bb71986208efb35f
malcozui <malcozui@gmail.com>
2022-05-28 09:00:23 +0200

    create data func and ReLU

--- NeuralNetworkAttempt/NeuralA.py
+++ NeuralNetworkAttempt/NeuralA.py
@@ -1,6 +1,18 @@
 import numpy as np
+import nnfs
 
-np.random.seed(0)
+nnfs.init()
+
+def create_data(points, classes):
+  X = np.zeros((points * classes), 2)
+  y = np.zeros(points * classes, dtype='uint8')
+  for class_number in range(classes):
+    ix = range(points * class_number, points * (class_number + 1))
+    r = np.linspace(0.0, 1, points)
+    t = np.linspace(class_number * 4, (class_number + 1) * 4, points) + np.random.randn(points) * 0.2
+    X[ix] = np.c_[r * np.sin(t * 2.5), r * np.cos(t * 2.5)]
+    y[ix] = class_number
+  return X, y
 
 X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
@@ -14,6 +26,12 @@ class Layer_Dense:
     self.biases = np.zeros((1, n_neurons))
   def forward(self, inputs):
     self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+  # Activation function using rectified linear equasion { x <= 0 = 0; x > 0 = x}
+  def forward(self, inputs):
+    self.output = np.maximum(0, inputs)
+    #applies the ReLU to the inputs
 
 layer1 = Layer_Dense(4,5)
 layer2 = Layer_Dense(5,2)
