328bfef7774e94e6bee68e006b44503273e93cfa
malcozui <malcozui@gmail.com>
2022-05-28 09:34:24 +0200

    did nothing

--- NeuralNetworkAttempt/soft_max_mathClass.py
+++ NeuralNetworkAttempt/soft_max_mathClass.py
@@ -1,12 +1,21 @@
-import numpy as np
-# the combination of exponentiation and normalisation is whta makes the soft max function
+import math
 layer_outputs = [4.8, 1.21, 2.385]
 
-E = np.e
-# first we exponentiate all the outputs
-exp_values = np.exp(layer_outputs)
-# then we divide every value by the sum of all the exponents to get the part of 1 they make up in whole.
-norm_values = exp_values / np.sum(exp_values)
+E = math.e
+
+
+exp_values = []
+
+for output in layer_outputs:
+    exp_values.append(E**output)
+
+print(exp_values)
+
+norm_base = sum(exp_values)
+norm_values = []
+
+for value in exp_values:
+    norm_values.append(value / norm_base)
 
 print(norm_values)
 print(sum(norm_values))
