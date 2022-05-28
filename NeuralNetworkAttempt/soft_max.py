dc13e5cdee6fc02a84a2c998d34b8532b692f122
malcozui <malcozui@gmail.com>
2022-05-28 09:44:26 +0200

    finished soft max

--- NeuralNetworkAttempt/soft_max.py
+++ NeuralNetworkAttempt/soft_max.py
@@ -1,21 +1,13 @@
-import math
-layer_outputs = [4.8, 1.21, 2.385]
+import numpy as np
 
-E = math.e
+layer_outputs = [[4.8, 1.21, 2.385],
+                 [8.9, 1.81, 0.2],
+                 [1.41, 1.051, 0.026]]
+                 
+exp_values = np.exp(layer_outputs)
 
+#print(np.sum(layer_outputs, axis=1, keepdims=True))
 
-exp_values = []
-
-for output in layer_outputs:
-    exp_values.append(E**output)
-
-print(exp_values)
-
-norm_base = sum(exp_values)
-norm_values = []
-
-for value in exp_values:
-    norm_values.append(value / norm_base)
+norm_values = exp_values / np.sum(exp_values, axis = 1, keepdims = True)
 
 print(norm_values)
-print(sum(norm_values))
